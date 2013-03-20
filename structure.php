9229ef6b8e94e2c0a228d6f8827a955f37871b8d
stdelorenzis <st.delorenzis@alerton.com.au>
2013-03-20 15:08:44 +1100

    fixed some ugly spacing

--- structure.php
+++ structure.php
@@ -1,31 +1,31 @@
 <?php
-	function get_header()
-	{
-                                                                                     
-		$headerHtml = "
-		<!DOCTYPE html>                                                               
-			<html>                                                                        
-				<head>                                                                      
-					<title>Heartbeat Training</title>                                      
-					<meta name=\"viewport\" content=\"width=device-width', initial-scale=1.0\"> 
-					<!--bootstrap-->                                                      
-					<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" media=\"screen\">    
-					<link href=\"css/bootstrap-responsive.min.css\" rel=\"stylesheet\">         
-					<link href=\"css/style.css\" rel=\"stylesheet\" media=\"screen\">             
-				</head>                                                                    
-				<body> ";   
-		echo $headerHtml;
-			
-	}
+function get_header()
+{
+                                                                                 
+	$headerHtml = "
+	<!DOCTYPE html>                                                               
+		<html>                                                                        
+			<head>                                                                      
+				<title>Heartbeat Training</title>                                      
+				<meta name=\"viewport\" content=\"width=device-width', initial-scale=1.0\"> 
+				<!--bootstrap-->                                                      
+				<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" media=\"screen\">    
+				<link href=\"css/bootstrap-responsive.min.css\" rel=\"stylesheet\">         
+				<link href=\"css/style.css\" rel=\"stylesheet\" media=\"screen\">             
+			</head>                                                                    
+		<body> ";   
+	echo $headerHtml;
+		
+}
 	
-	function get_footer()
-	{
-		$footerHtml = "
-			</body>
-		</html>";
-		
-		echo $footerHtml;
-		
+function get_footer()
+{
+	$footerHtml = "
+		</body>
+	</html>";
+	
+	echo $footerHtml;
+	
 
-	}
+}
 ?>
