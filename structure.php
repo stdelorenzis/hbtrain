66ccdffcd910f41c91899580e269e1c3800a3315
stdelorenzis <st.delorenzis@alerton.com.au>
2013-03-20 14:05:15 +1100

    get header function created

---
+++ structure.php
@@ -0,0 +1,29 @@
+<?php
+	function get_header()
+	{
+                                                                                     
+		$html = "<!DOCTYPE html>                                                               
+				<html>                                                                        
+					<head>                                                                      
+						<title>Heartbeat Training</title>                                      
+						<meta name=\"viewport\" content=\"width=device-width', initial-scale=1.0\"> 
+						<!--bootstrap-->                                                      
+						<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" media=\"screen\">    
+						<link href=\"css/bootstrap-responsive.min.css\" rel=\"stylesheet\">         
+						<link href=\"css/style.css\" rel=\"stylesheet\" media=\"Screen\">             
+					</head>                                                                    
+					<body> ";   
+		return $html;
+			
+	}
+	
+	function get_footer()
+	{
+
+	
+	
+			</body>
+		</html>
+
+	}
+?>
