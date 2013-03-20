fb4aa7dd4ac31cca939ad1ac320a205e9404b200
Vigneswara Baarathi <v.baarathi@alerton.com.au>
2013-03-20 10:05:15 +0800

    Added index file strucutre

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php echo "Hello World"; ?>
+<?php 
+	
+	require "db.php";
+	require "structure.php";
+	
+	
+	get_header();
+?>
+
+<!-- HTML CONTENT GOES HERE -->
+
+
+<?php
+
+	get_footer();
+	
+	?>
