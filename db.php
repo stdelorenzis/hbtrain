46129df6b35ac60bfe1c886a885daf8004335e22
Vigneswara Baarathi <v.baarathi@alerton.com.au>
2013-03-20 10:28:43 +0800

    DB connection file created.
    This code creates the connection between the web application and the
    database. Username and Password are stored in this file as well.

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+$objConnect = mysql_connect("localhost","optergyc_dash","password888") or die(mysql_error()); // Conect to MySQL
+$objDB = mysql_select_db("optergyc_dashboard");
+
+
+?>
