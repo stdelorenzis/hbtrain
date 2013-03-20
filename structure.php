8e801067d7c7bc7c34303b3529b6b5d3ff9b42d5
stdelorenzis <st.delorenzis@alerton.com.au>
2013-03-20 14:21:08 +1100

    fixed functions

--- structure.php
+++ structure.php
@@ -2,28 +2,30 @@
 	function get_header()
 	{
                                                                                      
-		$html = "<!DOCTYPE html>                                                               
-				<html>                                                                        
-					<head>                                                                      
-						<title>Heartbeat Training</title>                                      
-						<meta name=\"viewport\" content=\"width=device-width', initial-scale=1.0\"> 
-						<!--bootstrap-->                                                      
-						<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" media=\"screen\">    
-						<link href=\"css/bootstrap-responsive.min.css\" rel=\"stylesheet\">         
-						<link href=\"css/style.css\" rel=\"stylesheet\" media=\"Screen\">             
-					</head>                                                                    
-					<body> ";   
-		return $html;
+		$headerHtml = "
+		<!DOCTYPE html>                                                               
+			<html>                                                                        
+				<head>                                                                      
+					<title>Heartbeat Training</title>                                      
+					<meta name=\"viewport\" content=\"width=device-width', initial-scale=1.0\"> 
+					<!--bootstrap-->                                                      
+					<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" media=\"screen\">    
+					<link href=\"css/bootstrap-responsive.min.css\" rel=\"stylesheet\">         
+					<link href=\"css/style.css\" rel=\"stylesheet\" media=\"screen\">             
+				</head>                                                                    
+				<body> ";   
+		return $headerHtml;
 			
 	}
 	
 	function get_footer()
 	{
-
-	
-	
+		$footerHtml = "
 			</body>
-		</html>
+		</html>";
+		
+		return $footerHtml;
+		
 
 	}
 ?>
