2519f656d10932ddd3ea744267c3cbc0c4913a3d
stdelorenzis <st.delorenzis@alerton.com.au>
2013-03-20 15:15:05 +1100

    fixed the error to find css files

--- structure.php
+++ structure.php
@@ -9,9 +9,9 @@ function get_header()
 				<title>Heartbeat Training</title>                                      
 				<meta name=\"viewport\" content=\"width=device-width', initial-scale=1.0\"> 
 				<!--bootstrap-->                                                      
-				<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" media=\"screen\">    
-				<link href=\"css/bootstrap-responsive.min.css\" rel=\"stylesheet\">         
-				<link href=\"css/style.css\" rel=\"stylesheet\" media=\"screen\">             
+				<link href=\"bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\" media=\"screen\">    
+				<link href=\"bootstrap/css/bootstrap-responsive.min.css\" rel=\"stylesheet\">         
+				<link href=\"bootstrap/css/style.css\" rel=\"stylesheet\" media=\"screen\">             
 			</head>                                                                    
 		<body> ";   
 	echo $headerHtml;
