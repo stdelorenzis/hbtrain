51af38155f9d90092b1aab4e5c8a2b0aded84d21
stdelorenzis <st.delorenzis@alerton.com.au>
2013-03-21 10:16:02 +1100

    Add signup and cancel buttons

--- newMembers.php
+++ newMembers.php
@@ -13,21 +13,40 @@
   			<div class="span8 headerText"><h2>HEARTBEAT TRAINING CENTRE</h2></div>
    		</div>
 		<div class="row-fluid">
-			<div class="span12 text-center"><h3>Add New Members</h3></div>
+			<legend class="text-center"><h3>New Member</h3></legend>
 		</div>
 		<div class="row-fluid">
 			<div class="span3"></div>
 			<div class="span6">
 				<form class="form-horizontal">
 					<div class="control-group">
-						<label class="control-label" for="inputFname">FirstName</label>
+						<label class="control-label" for="inputFname">First Name</label>
 						<div class="controls">
 							<input class="input-xlarge" type="text" id="inputFname" placeholder="First Name">
+						</div>
+					</div>
+					<div class="control-group">
+						<label class="control-label" for="inputLname">Last Name</label>
+						<div class="controls">
+							<input class="input-xlarge" type="text" id="inputLname" placeholder="Last Name">
+							</p>
+							</p>
+							<button type="submit" class="btn btn-success">Sign Up</button>
+							<button class="btn btn-danger" onclick="cancel()">Cancel</button>
 						</div>
 					</div>
 				</form>
 			</div>
 			<div class="span3"></div>
+		</div>
+		</p>
+		<div class="row-fluid">
+			<div class="span3"></div>
+			<div class="span6">
+				<a class="btn btn-primary btn-block btn-large" href="index.php"><i class="icon-home icon-white"></i>&nbsp&nbspHome</a>
+			</div>
+			<div class="span3"></div>
+		</div>
 	</div>
 
 
