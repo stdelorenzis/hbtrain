43f1f17418ef833a995194568637310ffe0fa6bf
Vigneswara Baarathi <v.baarathi@alerton.com.au>
2013-03-20 11:34:35 +0800

    Added HTML sample text for training purposes

--- index.php
+++ index.php
@@ -9,6 +9,13 @@
 
 <!-- HTML CONTENT GOES HERE -->
 
+	<div class="container-fluid v-background">
+  		<div class="row-fluid header">
+  			<div class="span10"><h2>HEARTBEAT TRAINING CENTRE</h2></div>
+  			<div class="span2"><p>HELLO WORLD</p></div>
+  		</div>
+	</div>
+
 
 <?php
 
