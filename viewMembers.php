4cc594c6e21492f11f879595f265c5b2a4052f2b
stdelorenzis <st.delorenzis@alerton.com.au>
2013-03-21 12:11:04 +1100

    Ajax completed and extra nav buttons
    Javascript window.onload added to call ajax.
    Home and payment buttons below

--- viewMembers.php
+++ viewMembers.php
@@ -5,39 +5,75 @@
 	get_header();
 ?>
 
-	<script>
-	var xmlhttp;
-		
-		function showEntry()
+<script>
+var xmlhttp;
+	
+	function showEntry()
+	{
+		if (window.XMLHttpRequest) //for every other browser
 		{
-			if (window.XMLHttpRequest) //for every other browser
+			xmlhttp=new XMLHttpRequest();
+		}
+		else
+		{
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); //ie5 + 6
+		}
+		
+		xmlhttp.onreadystatechange=function()
+		{
+		if (xmlhttp.readyState == 4 && xmlhttp.status==200) //request finished and response  
 			{
-				xmlhttp=new XMLHttpRequest();
+				document.getElementById("memberTable").innerHTML = xmlhttp.responseText;
 			}
-			else
-			{
-				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); //ie5 + 6
-			}
-			
-			xmlhttp.onreadystatechange=function()
-			{
-			if (xmlhttp.readyState == 4 && xmlhttp.status==200) //request finished and response  
-				{
-					document.getElementById("memberTable").innerHTML = xmlhttp.responseText;
-				}
-			}
-			xmlhttp.open("POST","databaseChange.php",true); 
-			xmlhttp.send(); //sends request to opened page
-			
-			setTimeout("showEntry()",1000); //use timeout instead of setinterval so that it doesn't start the script over the other one.
-		}			
-	</script>
-
+		}
+		xmlhttp.open("POST","databaseChange.php",true); 
+		xmlhttp.send(); //sends request to opened page
+		
+		setTimeout("showEntry()",1000); //use timeout instead of setinterval so that it doesn't start the script over the other one.
+	}			
+</script>
+	
+<script type="text/javascript">
+	window.onload = showEntry();
+</script>
+	
+	
 	<div class="container-fluid">
+		<div class="row-fluid header">
+			<div class="span4"><img class="heart" src="img/heartgif.gif"/></div>
+  			<div class="span8 headerText"><h2>HEARTBEAT TRAINING CENTRE</h2></div>
+   		</div>
+		<div class="row-fluid">
+			<legend class="text-center"><h3>Registered Memebers</h3></legend>
+		</div>
 		<div class="row-fluid">
 			<div class="span3"></div>
 			<div class="span6" id="memberTable">
 			<!--autoupdate database appears here-->
+			</div>
+			<div class="span3"></div>
+		</div>
+		</p>
+		<div class="row-fluid">
+			<div class="span3"></div>
+			<div class="span6">
+				<a class="btn btn-success btn-block btn-large" href="newMembers.php"><i class="icon-user icon-white"></i>&nbsp&nbspRegister Now</a>
+			</div>
+			<div class="span3"></div>
+		</div>
+		</p>
+		<div class="row-fluid">
+			<div class="span3"></div>
+			<div class="span6">
+				<a class="btn btn-warning btn-block btn-large" href="index.php"><i class="icon-shopping-cart icon-white"></i>&nbsp&nbspPayment Options</a>
+			</div>
+			<div class="span3"></div>
+		</div>
+		</p>
+		<div class="row-fluid">
+			<div class="span3"></div>
+			<div class="span6">
+				<a class="btn btn-primary btn-block btn-large" href="index.php"><i class="icon-home icon-white"></i>&nbsp&nbspHome</a>
 			</div>
 			<div class="span3"></div>
 		</div>
