13b74592d3fc5fe84ee568583298830629cb8801
stdelorenzis <st.delorenzis@alerton.com.au>
2013-03-21 14:42:47 +1100

    File added for bootstrap js testing
    Just for training.

---
+++ bootstrapJsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+	require 'structure.php';
+	get_header();
+?>
+<script>
+
+</script>
+</p>
+<a href="#" class="btn" id="tooltipTest" rel="tooltip" data-content="this tooltip">hover over me</a>
+
+<?php
+	get_footer();
+?>
