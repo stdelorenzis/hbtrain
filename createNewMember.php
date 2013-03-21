80719befc04869597a9e152e2e9b0907d1b3eb70
stdelorenzis <st.delorenzis@alerton.com.au>
2013-03-21 11:01:45 +1100

    New Member processor created

--- createNewMember.php
+++ createNewMember.php
@@ -1,4 +1,15 @@
 <?php
 	require 'db.php';
 	require 'structure.php';
+	
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	
+	$sql = "INSERT INTO members(first_name, last_name, paid)
+			VALUES('$fname', '$lname', 0)";
+	mysql_query($sql);
+			
+	mysql_close($objConnect); //close db
+	
+	header("Location: index.php");
 ?>
