13710a975b65a3065e08ae1b68fe5c5aa5e982a9
stdelorenzis <st.delorenzis@alerton.com.au>
2013-03-25 12:18:53 +1100

    Added toggle buttons to ohs checklist js

--- structure.php
+++ structure.php
@@ -47,119 +47,145 @@ function get_ohsScript()
 		
 		//question 1
 		var question1 = "<div class=\"show\" id=\"question1\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 1%\"></div>\n"
-						+		"</div>\n"
-						+	 	"<a id=\"btnQ1\" class=\"btn btn-primary pull-right\">Next</a>\n"
-						+	"</div>";
+						+"	<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 1%\"></div>\n"
+						+"	</div>\n"
+						+"	<div class=\"row-fluid\">\n"
+						+"		<div class=\"span12\">\n"
+						+"			<p class=\"text-center\">Are you working in the office today?</p>\n"
+						+" 		</div>\n"
+						+"	</div>\n"
+						+" 	<div class=\"row-fluid\">\n"
+						+" 		<div class=\"span12 text-center btn-group\" data-toggle=\"buttons-radio\">\n"
+						+" 			<button type=\"button\" class=\"btn btn-large\" id=\"workingOfficeBtnYes\"> Yes </button>\n"
+						+"			<button type=\"button\" class=\"btn btn-large\" id=\"workingOfficeButtonNo\"> No </button>\n"
+						+"		</div>\n"
+						+"	</div>\n"
+						+"	<a id=\"btnQ1\" class=\"btn btn-primary pull-right\">Next</a>\n"
+						+"</div>";
 			
-			
+		/*				radio buttons while testing buttons		
+						+"			<input type=\"radio\" name=\"office\" id=\"workingOfficeYes\" value=\"yes\"> Yes </input>\n"
+						+" 			&nbsp&nbsp&nbsp\n"
+						+" 			<input type=\"radio\" name=\"office\" id=\"workingOfficeNo\" value=\"no\"> No </input>\n"	*/
+						
 		//question 2
 		var question2 =  "<div class=\"hide\" id=\"question2\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 8.3%\"></div>\n"
-						+ 	 	"</div>\n"
-						+ 		"<a id=\"btnPrev2\" class=\"btn btn-primary pull-left\">Previous</a>\n"
-						+	 	"<a id=\"btnQ2\" class=\"btn btn-primary pull-right\">Next</a>\n"
-						+	"</div>\n";
+						+"		<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 8.3%\"></div>\n"
+						+"		</div>\n"
+						+"	<div class=\"row-fluid\">\n"
+						+"		<div class=\"span12\">\n"
+						+"			<p class=\"text-center\">What is your job number?</p>\n"
+						+" 		</div>\n"
+						+"	</div>\n"
+						+" 	<div class=\"row-fluid\">\n"
+						+" 		
+						+" 		
+						+"		
+						+"		
+						+"	</div>\n"
+						+"		<a id=\"btnPrev2\" class=\"btn btn-primary pull-left\">Previous</a>\n"
+						+"		<a id=\"btnQ2\" class=\"btn btn-primary pull-right\">Next</a>\n"
+						+"</div>\n";
 		
 		//question 3	
 		var question3 =	"<div class=\"hide\" id=\"question3\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 16.6%\"></div>\n"
-						+ 	 	"</div>\n"
-						+ 		"<a id=\"btnPrev3\" class=\"btn btn-primary pull-left\">Previous</a>\n"
-						+	 	"<a id=\"btnQ3\" class=\"btn btn-primary pull-right\">Next</a>\n"
-						+	"</div>\n";
+						+"	<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 16.6%\"></div>\n"
+						+"	</div>\n"
+						+"	<a id=\"btnPrev3\" class=\"btn btn-primary pull-left\">Previous</a>\n"
+						+"	<a id=\"btnQ3\" class=\"btn btn-primary pull-right\">Next</a>\n"
+						+"</div>\n";
 		
 		//question 4
 		var question4 =	"<div class=\"hide\" id=\"question4\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 25%\"></div>\n"
-						+ 	 	"</div>\n"
-						+ 		"<a id=\"btnPrev4\" class=\"btn btn-primary pull-left\">Previous</a>\n"
-						+	 	"<a id=\"btnQ4\" class=\"btn btn-primary pull-right\">Next</a>\n"
-						+	"</div>\n";
+						+"	<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 25%\"></div>\n"
+						+"		</div>\n"
+						+"	<a id=\"btnPrev4\" class=\"btn btn-primary pull-left\">Previous</a>\n"
+						+"	<a id=\"btnQ4\" class=\"btn btn-primary pull-right\">Next</a>\n"
+						+"</div>\n";
 
 		//question 5
 		var question5 =	"<div class=\"hide\" id=\"question5\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 33.3%\"></div>\n"
-						+ 	 	"</div>\n"
-						+ 		"<a id=\"btnPrev5\" class=\"btn btn-primary pull-left\">Previous</a>\n"
-						+	 	"<a id=\"btnQ5\" class=\"btn btn-primary pull-right\">Next</a>\n"
-						+	"</div>\n";
+						+"	<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 33.3%\"></div>\n"
+						+"	</div>\n"
+						+"	<a id=\"btnPrev5\" class=\"btn btn-primary pull-left\">Previous</a>\n"
+						+"	<a id=\"btnQ5\" class=\"btn btn-primary pull-right\">Next</a>\n"
+						+"</div>\n";
 						
 		//question 6
 		var question6 =	"<div class=\"hide\" id=\"question6\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 41.6%\"></div>\n"
-						+ 	 	"</div>\n"
-						+ 		"<a id=\"btnPrev6\" class=\"btn btn-primary pull-left\">Previous</a>\n"
-						+	 	"<a id=\"btnQ6\" class=\"btn btn-primary pull-right\">Next</a>\n"
-						+	"</div>\n";
+						+"	<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 41.6%\"></div>\n"
+						+"	</div>\n"
+						+"	<a id=\"btnPrev6\" class=\"btn btn-primary pull-left\">Previous</a>\n"
+						+"	<a id=\"btnQ6\" class=\"btn btn-primary pull-right\">Next</a>\n"
+						+"</div>\n";
 		
 		//question 7
 		var question7 =	"<div class=\"hide\" id=\"question7\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 50%\"></div>\n"
-						+ 	 	"</div>\n"
-						+ 		"<a id=\"btnPrev7\" class=\"btn btn-primary pull-left\">Previous</a>\n"
-						+	 	"<a id=\"btnQ7\" class=\"btn btn-primary pull-right\">Next</a>\n"
-						+	"</div>\n";
+						+"	<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 50%\"></div>\n"
+						+"	</div>\n"
+						+"	<a id=\"btnPrev7\" class=\"btn btn-primary pull-left\">Previous</a>\n"
+						+"	<a id=\"btnQ7\" class=\"btn btn-primary pull-right\">Next</a>\n"
+						+"</div>\n";
 		
 		//question 8
 		var question8 =	"<div class=\"hide\" id=\"question8\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 58.3%\"></div>\n"
-						+ 	 	"</div>\n"
-						+ 		"<a id=\"btnPrev8\" class=\"btn btn-primary pull-left\">Previous</a>\n"
-						+	 	"<a id=\"btnQ8\" class=\"btn btn-primary pull-right\">Next</a>\n"
-						+	"</div>\n";
+						+"	<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 58.3%\"></div>\n"
+						+"	</div>\n"
+						+"	<a id=\"btnPrev8\" class=\"btn btn-primary pull-left\">Previous</a>\n"
+						+"	<a id=\"btnQ8\" class=\"btn btn-primary pull-right\">Next</a>\n"
+						+"</div>\n";
 		
 		//question 9
 		var question9 =	"<div class=\"hide\" id=\"question9\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 66.6%\"></div>\n"
-						+ 	 	"</div>\n"
-						+ 		"<a id=\"btnPrev9\" class=\"btn btn-primary pull-left\">Previous</a>\n"
-						+	 	"<a id=\"btnQ9\" class=\"btn btn-primary pull-right\">Next</a>\n"
-						+	"</div>\n";
+						+"	<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 66.6%\"></div>\n"
+						+"	</div>\n"
+						+"	<a id=\"btnPrev9\" class=\"btn btn-primary pull-left\">Previous</a>\n"
+						+"	<a id=\"btnQ9\" class=\"btn btn-primary pull-right\">Next</a>\n"
+						+"</div>\n";
 						
 		//question 10
-		var question10 =	"<div class=\"hide\" id=\"question10\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 74.8%\"></div>\n"
-						+ 	 	"</div>\n"
-						+ 		"<a id=\"btnPrev10\" class=\"btn btn-primary pull-left\">Previous</a>\n"
-						+	 	"<a id=\"btnQ10\" class=\"btn btn-primary pull-right\">Next</a>\n"
-						+	"</div>\n";
+		var question10 ="<div class=\"hide\" id=\"question10\">\n"
+						+"	<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 74.8%\"></div>\n"
+						+"	</div>\n"
+						+"	<a id=\"btnPrev10\" class=\"btn btn-primary pull-left\">Previous</a>\n"
+						+"	<a id=\"btnQ10\" class=\"btn btn-primary pull-right\">Next</a>\n"
+						+"</div>\n";
 						
 		//question 11
-		var question11 =	"<div class=\"hide\" id=\"question11\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 83.3%\"></div>\n"
-						+ 	 	"</div>\n"
-						+ 		"<a id=\"btnPrev11\" class=\"btn btn-primary pull-left\">Previous</a>\n"
-						+	 	"<a id=\"btnQ11\" class=\"btn btn-primary pull-right\">Next</a>\n"
-						+	"</div>\n";
+		var question11 ="<div class=\"hide\" id=\"question11\">\n"
+						+"	<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 83.3%\"></div>\n"
+						+"	</div>\n"
+						+"	<a id=\"btnPrev11\" class=\"btn btn-primary pull-left\">Previous</a>\n"
+						+"	<a id=\"btnQ11\" class=\"btn btn-primary pull-right\">Next</a>\n"
+						+"</div>\n";
 		
 		//question 12
-		var question12 =	"<div class=\"hide\" id=\"question12\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 91.4%\"></div>\n"
-						+ 	 	"</div>\n"
-						+ 		"<a id=\"btnPrev12\" class=\"btn btn-primary pull-left\">Previous</a>\n"
-						+	 	"<a id=\"btnQ12\" class=\"btn btn-primary pull-right\">Next</a>\n"
-						+	"</div>\n";
+		var question12 ="<div class=\"hide\" id=\"question12\">\n"
+						+"	<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 91.4%\"></div>\n"
+						+"	</div>\n"
+						+"	<a id=\"btnPrev12\" class=\"btn btn-primary pull-left\">Previous</a>\n"
+						+"	<a id=\"btnQ12\" class=\"btn btn-primary pull-right\">Next</a>\n"
+						+"</div>\n";
 		//question 13
-		var question13 =	"<div class=\"hide\" id=\"question13\">\n"
-						+		"<div class=\"progress\">\n"
-						+			"<div class=\"bar bar-success\" style=\"width: 100%\"></div>\n"
-						+ 	 	"</div>\n"
-						+ 		"<a id=\"btnPrev13\" class=\"btn btn-primary pull-left\">Previous</a>\n"
-						+	 	"<a id=\"btnQ13\" class=\"btn btn-success pull-right\">Save</a>\n"
-						+	"</div>\n";
+		var question13 ="<div class=\"hide\" id=\"question13\">\n"
+						+"	<div class=\"progress\">\n"
+						+"		<div class=\"bar bar-success\" style=\"width: 100%\"></div>\n"
+						+"	</div>\n"
+						+"	<a id=\"btnPrev13\" class=\"btn btn-primary pull-left\">Previous</a>\n"
+						+"	<a id=\"btnQ13\" class=\"btn btn-success pull-right\">Save</a>\n"
+						+"</div>\n";
 		
 		//onload
 		$("#question").append(question1,question2,question3,question4,question5,question6,question7,question8,question9,question10,question11,question12,question13);
@@ -319,7 +345,12 @@ function get_ohsScript()
 			
 			$("#question13").hide();
 			$("#question12").show();
-		});';
+		});
+		
+		
+		
+		';
+	
 	
 
 	echo $OHSscript;
