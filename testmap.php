33c0bce1d1422d4652786dc04c30088aa55af8c7
stdelorenzis <st.delorenzis@alerton.com.au>
2013-03-26 17:07:40 +1100

    Testing the google map api
    Not working yet

--- testmap.php
+++ testmap.php
@@ -8,32 +8,52 @@ get_header();
 
 $sql = "SELECT * FROM checkin";
 $res = mysql_query($sql);
+
+while ($result = mysql_fetch_array($res))
+{
+	
+}
 ?>
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBR2M-GicedlQIdxvVdKuzLS61KA6_jKYI&sensor=false"></script>
 <script type="text/javascript">
 var geocoder;
 var map;
 var infowindow = new google.maps.InfoWindow();
-var marker;
+var locations = [	
+['Melbourne', -37.7833, 144.9667, 1],
+['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+['Maroubra Beach', -33.950198, 151.259302, 1]
+];
 function initialize(){
-	geocode = new google.maps.Geocoder();
+	geocoder = new google.maps.Geocoder();
 	var latlng = new google.maps.LatLng(-37.7833, 144.9667);
 	var mapOptions = {
 		zoom: 8,
-		center: latlng
+		center: latlng,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
-	map = new google.maps.Map(document.getElementById("map-canvas"), mapoptions);
+var	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
 };
+var marker, i;
 
-function codeLatLng(){
-	var input = document.getElementById("latlng").value;
-	var latlngstr = input.split(",",2);
-	var lat = parseFloat(latlngStr[0]);
-	var lng = parseFloat(latlngStr[1]);
-	var latlng = new google.maps.LatLng(lat, lng);
-	geocoder.geocode({'latLng': latlng}), function(results, status) {
+for (i = 0; i < locations.length; i++){
+	marker = new google.maps.Marker({
+		position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+		map: map
+	});
+	
+	google.maps.event.addListener(marker, 'click', (function(marker, i) {
+		return function(){
+			infowindow.setContent(locations[i][0]);
+			infowindow.open(map, marker);
+		}
+	})(marker, i));
+}
+
+
+/*function codeLatLng(){
+		geocoder.geocode({'latLng': latlng}), function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK){
 			if(results[0]){
 				map.setZoom(11);
@@ -48,12 +68,12 @@ function codeLatLng(){
 			alert("Geocoder failed due to: "+status);
 		}
 	});
-};
-
+};*/
+window.onload = initialize();
 </script>
 
 <div class="row-fluid">
-	<div class="span12" id="map-canvas">
+	<div class="span12" id="map-canvas" style="width: 500px; height: 500px;"> <!--a is a null error-->
 	</div>
 </div>
 
