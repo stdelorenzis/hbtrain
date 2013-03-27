4a4a8af714b02bf01931d2f5667552c897a95158
stdelorenzis <st.delorenzis@alerton.com.au>
2013-03-27 15:50:11 +1100

    function to show map

--- structure.php
+++ structure.php
@@ -697,32 +697,36 @@ function createMap()
 	var geocoder;
 	var map;
 	
-	var	map = new google.maps.Map(document.getElementById("map-canvas"), {
-			zoom: 10,
-			center: new google.maps.LatLng(-37.7833, 144.9667),
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-	});
-
-
-	var infowindow = new google.maps.InfoWindow();
-	geocoder = new google.maps.Geocoder();
-
-	var marker, i;
-
-	for (i = 0; i < locations.length; i++){
-		marker = new google.maps.Marker({
-			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-			map: map
+		var	map = new google.maps.Map(document.getElementById("map-canvas"), {
+				zoom: 10,
+				center: new google.maps.LatLng(-37.7833, 144.9667),
+				mapTypeId: google.maps.MapTypeId.ROADMAP
 		});
+
+
+		var infowindow = new google.maps.InfoWindow();
+		geocoder = new google.maps.Geocoder();
+
+		var marker, i;
+
 		
-		google.maps.event.addListener(marker, "click", (function(marker, i) {
-			return function(){
-				infowindow.setContent(locations[i][0]);
-				infowindow.open(map, marker);
-			}
-		})(marker, i));
-		
-	}
+		for (i = 0; i < locations.length; i++){
+			marker = new google.maps.Marker({
+				position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+				map: map
+			});
+			
+			google.maps.event.addListener(marker, "click", (function(marker, i) {
+				return function(){
+					infowindow.setContent(locations[i][0]);
+					infowindow.open(map, marker);
+				}
+			})(marker, i));
+			
+		}
+	
+	
+	
 	';
 	
 	echo $createMap;
