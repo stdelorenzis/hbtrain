87be9bad296862b739f83c22aa88bd407ffdc325
stdelorenzis <st.delorenzis@alerton.com.au>
2013-04-02 09:36:33 +1100

    Dashboard checked in Ajax
    Not working yet, no db to collect from.

---
+++ hbCheckedIn.php
@@ -0,0 +1,11 @@
+<?php
+//select from db and show in table
+//tr already created. Just need cells.
+/*
+<td>Last name</td>
+<td>First name</td>
+<td>Time</td>
+<td>Location</td>
+<td>status</td>
+*/
+?>
