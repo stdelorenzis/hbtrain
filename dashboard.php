6586da0aaf271bed6bf00a13f88a79e52543ca78
stdelorenzis <st.delorenzis@alerton.com.au>
2013-04-02 09:37:48 +1100

    Ajax added for date time and checked in table

--- dashboard.php
+++ dashboard.php
@@ -2,18 +2,62 @@
 	require 'db.php';
 	require 'structure.php';
 	
-	get_header();
-	date_default_timezone_set('Australia/Melbourne');
+	date_default_timezone_set("Australia/Melbourne");
 	$time = date("H:i:s");
 	$date = date("Y/m/d");
+	
+	get_header();
+
 ?>
+	<script>
+	//ajax
+	var xmlhttp;
+	var xmlhttpDateTime;
+		
+		function showEntry()
+		{
+			if (window.XMLHttpRequest)
+			{
+				xmlhttp=new XMLHttpRequest();
+				xmlhttpDateTime = new XMLHttpRequest();
+			}
+			else
+			{
+				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); //ie5 + 6
+				xmlhttpDateTime = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			
+			xmlhttp.onreadystatechange=function()
+			{
+			if (xmlhttp.readyState == 4 && xmlhttp.status==200) //request finished and response  
+				{
+					document.getElementById("hbCcheckedIn").innerHTML = xmlhttp.responseText;
+				}
+			}
+			xmlhttp.open("POST","hbCheckedIn.php",true); 
+			xmlhttp.send(); //sends request to opened page
+			
+			xmlhttpDateTime.onreadystatechange=function(){
+				if (xmlhttpDateTime.readyState == 4 && xmlhttpDateTime.status==200)
+				{
+					document.getElementById("dateTime").innerHTML = xmlhttpDateTime.responseText;
+				}
+			}
+			xmlhttpDateTime.open("POST","dateTime.php",true);
+			xmlhttpDateTime.send();
+			
+			setTimeout("showEntry()",1000); //use timeout instead of setinterval so that it doesn't start the script over the other one.
+		}			
+
+	window.onload = showEntry();
+	</script>
 	<div class="container-fluid v-background s-container">
   		<div class="row-fluid header headerText">
 			<div class="span1"></div>
 			<div class="span5">
 				<h2>Heartbeat 1.0</h2>
 				</br>
-				<h4><?php echo $date. "   " .$time?></h4>
+				<h4 id="dateTime"></h4>
 			</div>
 			<div class="span5"><img class="pull-right img-rounded" src="img/logo.png"/></div>
 			<div class="span1"></div>
@@ -24,23 +68,13 @@
 			<div class="span10">
 			<table class="table table-striped table-bordered">
 				<tr>
-					<th>
-						Last Name
-					</th>
-					<th>
-						First Name
-					</th>
-					<th>
-						Time
-					</th>
-					<th>
-						Loaction
-					</th>
-					<th>
-						Status
-					</th>
+					<th>Last Name</th>
+					<th>First Name</th>
+					<th>Time</th>
+					<th>Loaction</th>
+					<th>Status</th>
 				</tr>
-				<tr>
+				<tr id="hbCheckedIn">
 				</tr>
 			</table>
 			</div>
