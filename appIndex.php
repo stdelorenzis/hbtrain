bb0bdfa22dc8f890f0b4440e8acac32631558127
stdelorenzis <st.delorenzis@alerton.com.au>
2013-04-02 12:40:21 +1100

    app home page
    Nothing on it yet

---
+++ appIndex.php
@@ -0,0 +1,14 @@
+<?php
+	require 'db.php';
+	require 'structure.php';
+	
+	get_header();
+?>
+	<div class="container-fluid v-background s-container">
+  		<div class="row-fluid header text-center">
+  			<div class="span12 headerText"><h2>Heartbeat Training Centre</h2></div>
+   		</div>
+	</div>
+<?php
+	get_footer();
+?>
