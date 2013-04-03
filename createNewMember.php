075577209ffa30c04581ca4bf9e3fd3864912f5d
Stephanie <st.delorenzis@alerton.com.au>
2013-04-03 16:27:28 +1100

    sql and php to create unique user

--- createNewMember.php
+++ createNewMember.php
@@ -5,11 +5,31 @@
 	$fname = $_POST['fname'];
 	$lname = $_POST['lname'];
 	
-	$sql = "INSERT INTO members(first_name, last_name, paid)
-			VALUES('$fname', '$lname', 0)";
-	mysql_query($sql);
-			
-	mysql_close($objConnect); //close db
+	$fname_split = chunk_split($fname,1," ");
+	$fname_split2 = chunk_split($fname,2," ");
+	$fname_arr = explode(" ", $fname_split);
+	$username = strtolower($fname_arr[0].$lname);
+		
+	$sql = "SELECT * FROM members WHERE username = '$username'";
+	$res = mysql_query($sql);
 	
-	header("Location: index.php");
+	if (mysql_num_rows($res)!=0) //if username exists in db create a new one
+	{
+		$fname_arr = explode(" ", $fname_split2);
+		$username = strtolower($fname_arr[0].$lname);
+	}
+	
+	$sql = "INSERT INTO members(first_name, last_name, paid, username)
+				VALUES('$fname', '$lname', 0, '$username')";
+	$res = mysql_query($sql);
+		
+	if($res)
+	{
+		mysql_close($objConnect); //close db
+		header("Location: index.php");
+	}
+	else
+		echo mysql_error();
+		
+	
 ?>
--- newMembers.php
+++ newMembers.php
@@ -4,9 +4,36 @@
 	require 'structure.php';
 	
 	get_header();
+	
+	
 ?>
-
-
+	<script>
+		$(document).ready(function(){
+			$("#newMemberBtn").click(function(){
+				var fname = $('#inputFname').val();
+				var lname = $('#inputLname').val();
+				alert(fname + ' ' + lname);
+			<?php 	
+				$fname = print("fname");
+				$lname = print("lname");
+				print ("alert($fname + ' ' + $lname);\n
+						return;");
+					/*$sql = "SELECT * FROM members WHERE first_name = fname and last_name = lname";
+					$res = mysql_query($sql);
+					
+					if (mysql_num_row($res) != 0)
+						{
+				?>
+						alert("User exists");
+						return false;
+						
+				<?php
+						}*/
+				?>
+				
+			});
+		});
+	</script>
 	<div class="container-fluid v-background s-container">
   		<div class="row-fluid header">
 			<div class="span4"><img class="heart" src="img/heartgif.gif"/></div>
@@ -31,11 +58,14 @@
 							<input class="input-xlarge" type="text" name="lname" id="inputLname" placeholder="Last Name">
 							</p>
 							</p>
-							<button type="submit" class="btn btn-success">Sign Up</button>
+							<button class="btn btn-success">Sign Up</button> <!--type="submit" id="newMemberBtn"  -->
 							<a class="btn btn-danger" href="index.php">Cancel</a>
 						</div>
 					</div>
 				</form>
+				<div>
+					<button id="newMemberBtn" class="btn btn-warning">check</button>
+				</div>
 			</div>
 			<div class="span3"></div>
 		</div>
