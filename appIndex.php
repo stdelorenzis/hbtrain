4e7762cc790d1bddd9dad239ef7c091c9120ff21
stdelorenzis <st.delorenzis@alerton.com.au>
2013-04-03 11:05:12 +1100

    The main page for the app

--- appIndex.php
+++ appIndex.php
@@ -6,9 +6,26 @@
 ?>
 	<div class="container-fluid v-background s-container">
   		<div class="row-fluid header text-center">
-  			<div class="span12 headerText"><h2>Heartbeat Training Centre</h2></div>
+  			<div class="span12 headerText"><h3>Alerton</p>Heartbeat</h3></div>
    		</div>
+		<div class="row-fluid text-center">
+			<div class="span10 offset1">
+				<button class="btn btn-primary btn-large checkinBtn span12" type="button">Tap To </p> Check In 
+				</button>
+			</div>
+		</div>
+		</br>
+		<div class="row-fluid">
+			<div class="span10 offset1 text-center">
+				FirstName LastName
+			</div>
+		</div>		
+		<div class="row-fluid">
+			<div class="span10 offset1 text-center">
+				Location Identified: SiteName
+			</div>
+		</div>
 	</div>
 <?php
-	get_footer();
+	get_app_footer();
 ?>
