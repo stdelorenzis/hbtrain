c0256c1d416aac4235b3efb1263d1cfcacfee347
Stephanie <st.delorenzis@alerton.com.au>
2013-04-04 15:51:20 +1100

    new inputs to take coords to appcheckout

--- appIndex.php
+++ appIndex.php
@@ -9,28 +9,33 @@ $(document).ready(function(){
 <?php 
 	geo();
 ?>
+
 });
 </script>
 	<div class="container-fluid v-background s-container">
   		<div class="row-fluid header text-center">
   			<div class="span12 headerText"><h3>Heartbeat</h3></div>
    		</div>
-		<div class="row-fluid text-center">
-			<div class="span10 offset1">
-				<button class="btn btn-primary btn-large checkinBtn span12" type="button">Tap To </p> Check In 
-				</button>
+		<div class="row-fluid">
+			<div class="span4 text-center">
+				<form method="post" action="appCheckOut.php">
+					<a id="checkInBtn" class="btn btn-primary btn-large checkinBtn disabled"><p id="checkInTxt">Tap to Check In</p></a>
+				</form>
 			</div>
 		</div>
 		</br>
 		<div class="row-fluid">
-			<div class="span10 offset1 text-center" id="personName">
+			<div class="span10 offset1 text-center">
 				FirstName LastName
 			</div>
 		</div>		
 		<div class="row-fluid">
-			<div class="span12 text-center">
-				Location Identified: </p>
-				<span id="long"></span>&nbsp <span id="lat"></span>
+			<div class="span12 text-center" >
+				<form id="locationForm" method="post" action="appCheckOut.php">
+					<p id="locationP"></p>
+					<span id="long"></span>&nbsp<span id="lat"></span>
+					<input type="hidden" name="longIn" id="longIn"/><input type="hidden" name="latIn" id="latIn"/>
+				</form>
 			</div>
 		</div>
 	</div>
