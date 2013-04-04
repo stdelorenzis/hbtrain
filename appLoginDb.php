496068f922803b18bbebbcb73bab100f08637e81
Stephanie <st.delorenzis@alerton.com.au>
2013-04-04 15:56:25 +1100

    placeholder for sql

---
+++ appLoginDb.php
@@ -0,0 +1,16 @@
+<?php
+	//when db works
+	/*
+		find user
+		sign them in
+		start their session
+		change their status to "Checked in"
+		change their location to lat/long
+		add to known locations
+		
+		can be put under structure as a function save files.
+	*/
+
+	header("Location: appIndex.php");
+
+?>
